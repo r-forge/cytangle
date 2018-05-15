9285c9ee5a772ab7460891a623aab57ea29f5c9c
krcoombes (Kevin Coombes) <kevin.r.coombes@gmail.com>
2018-05-15 11:20:12 +0000

    started writing code to read-write FCS as collections of RDA files.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/cytangle@10 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/Cytangle/R/01-FCSconvert.R
+++ pkg/Cytangle/R/01-FCSconvert.R
@@ -2,3 +2,29 @@
 ###
 ### Function to convert from unified SPADE-clustered FCS files to
 ### node-separated chunks to be stored as Rdata files.
+
+readTangleRDA <- function(patient, node="all", path=".") {
+  loc <- file.path(path, patient)
+  if(!dir.exists(loc)) {
+    stop("Cannote find directory:", loc, "\n")
+  }
+  if (node == "all") {
+    fils <- dir(loc, pattern="node*Rda")
+  } else {
+    fils <- paste("node", node, ".Rda", sep="")
+  }
+  if (length(fils) == 0) {
+    stop("")
+  }
+  val <- NULL
+  for (fil %in% fils) {
+    load(file.path(loc, fil))
+    if (is.null(val)) {
+      val <- tempmat
+    } else {
+      val <- rbid(val, tepMat)
+    }
+  rm(tempmat)
+  }
+  val
+}
