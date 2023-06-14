c7a602e916c379b7e2e942049326b781664c157a
krcoombes (Kevin Coombes) <kevin.r.coombes@gmail.com>
2023-06-14 14:04:40 +0000

    Added "LoopFeature" objects to look for significance of features changing around a loop. Also cleaned up manual pages.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/cytangle@40 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/Mender/R/08-loopFeature.R
@@ -0,0 +1,74 @@
+## 08-loopFeature.R
+## Copyright (C) 2023 Kevin R. Coombes, RB McGee, and Jake Reed
+## LICENSE: Perl Artistic License 2.0
+
+setClass("LoopFeature",
+         slots = c(
+           data = "matrix",
+           fitted = "matrix",
+           RSS = "numeric",
+           V = "numeric",
+           Kstat = "numeric"
+         ))
+
+LoopFeature <- function(circMeans) {
+  steps <- 360/nrow(circMeans)
+  theta <- seq(steps, 360, steps)/180*pi
+  eta <- function(param, y) {
+    a <- param[1]
+    b <- param[2]
+    h <- param[3]
+    r <- h + a*cos(theta) + b*sin(theta)
+    x <- (r-y)^2
+    sum(x, na.rm = TRUE)
+  }
+
+  fitted <- sapply(colnames(circMeans), function(N) {
+    opi <- optim(c(0, 1, 1), eta, y=circMeans[,N])
+    fit <- opi$par[3] + opi$par[1]*cos(theta) + opi$par[2]*sin(theta)
+    fit
+  })
+
+  RSS <- apply((fitted - circMeans)^2, 2, sum, na.rm = TRUE)
+  spread <- apply(rbind(circMeans, fitted), 2, function(X) {
+    diff(range(X, na.rm = TRUE))
+  })
+  V <- apply(circMeans, 2, sd, na.rm=TRUE)^2
+  Kstat <- RSS/V
+  new("LoopFeature",
+      data = circMeans,
+      fitted = fitted,
+      RSS = RSS,
+      V = V,
+      Kstat = Kstat
+      )
+}
+
+setMethod("plot", c("LoopFeature", "missing"), function(x, y, ...) {
+  plot(x, colnames(x@fitted))
+})
+
+
+setMethod("plot", c("LoopFeature", "character"), function(x, y, ...) {
+  if (missing(y) | (length(y) == 1 && y == "all")) y <- colnames(x@fitted)
+  for (N in y) {
+    rg <- range(c(x@data[,N], x@fitted[,N]), na.rm = TRUE)
+    plot(x@data[,N], pch = 16, main = N, ylim = rg)
+    title(sub = round(x@RSS[N], 3))
+    points(x@fitted[, N], type = "b")
+  }
+  invisible(x)
+})
+
+setMethod("image", "LoopFeature", function(x, ...) {
+  oo <- order(apply(x@fitted, 2, which.max))
+  chock <- x@fitted[, oo]
+  shock <- scale(chock)
+  steps <- 360/nrow(x@data)
+  image(seq(0, 345, steps), 1:ncol(shock), shock,
+        xlab = "degrees", yaxt = "n", ylab = "", ...)
+  mtext(colnames(shock), side = 4, at = 1:ncol(shock), line = 1, las = 2)
+  invisible(x)
+})
+
+
---
+++ pkg/Mender/tests/test-LF.R
@@ -0,0 +1,16 @@
+library(Mender)
+data(CLL)
+view <- cmdscale(daisydist)
+circular <- angleMeans(view, ripdiag, NULL, clinical)
+lf <- LoopFeature(circular)
+sort(lf@Kstat)
+opar <- par(mai = c(0.82, 0.2, 0.82, 1.82))
+image(lf, main = "Clinical Factors in CLL")
+par(opar)
+
+plot(lf, "mutation.status")
+opar <- par(mfcol = c(2,2))
+plot(lf, c("Serum.beta.2.microglobulin", "Hemoglobin",
+           "Rai.Stage", "LogLDH"))
+par(opar)
+
