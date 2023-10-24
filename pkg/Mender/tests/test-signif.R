7289c2425d2d6d709be99f310befeb3790b86d5c
krcoombes (Kevin Coombes) <kevin.r.coombes@gmail.com>
2023-10-24 21:19:50 +0000

    Added a test script for confidence band computations using Gumbel distribution.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/cytangle@50 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/Mender/tests/test-signif.R
@@ -0,0 +1,34 @@
+library(Mender)
+library(TDA)
+set.seed(54321)
+theta <- seq(0, 359, by = 8) * pi/180
+L <- length(theta)
+x <- cos(theta) + rnorm(L, 0, 0.2)
+y <- sin(theta) + rnorm(L, 0, 0.2)
+z <- matrix(rnorm(2*L, 1, 0.5), ncol = 2)
+joint <- rbind(as.matrix(data.frame(x, y)), z)
+ripper <- ripsDiag(joint, maxdimension=1, maxscale = 3,
+                   dist = "euclidean", library = "Dionysus",
+                   location = TRUE)
+RD <- ripper$diagram
+gum <- Mender:::process(RD)
+tack <- Mender:::test(gum)
+cb <- Mender:::con_band(RD)
+
+set.seed(97531)
+jag <- jitter(joint)
+ripped <- ripsDiag(jag, maxdimension=1, maxscale = 3,
+                   dist = "euclidean", library = "Dionysus",
+                   location = TRUE)
+RD2 <- ripped$diagram
+gum <- Mender:::process(RD)
+tack <- Mender:::test(gum)
+cb2 <- Mender:::con_band(RD2)
+
+exp(cb)
+exp(cb2)
+
+opar <- par(mfrow=c(1,2))
+plot(RD, band = cb)
+plot(RD2, band = cb2)
+par(opar)
