678b13a57ca51c971a58675b2fb8aa5e4e0abc9b
krcoombes (Kevin Coombes) <kevin.r.coombes@gmail.com>
2024-05-01 10:30:44 +0000

    Put together q plotting vignette. Started adding a "hexagon" node shape
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/cytangle@117 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/WayFindR/R/09-ellipse.R
+++ pkg/WayFindR/R/09-ellipse.R
@@ -19,3 +19,28 @@ igraphEllipse <- function(coords, v=NULL, params) {
                          col = vertex.color)
 }
 
+## Function for plotting a hexagonal node
+igraphHexagon <- function(coords, v=NULL, params) {
+  vertex.color <- params("vertex", "color")
+  if (length(vertex.color) != 1 && !is.null(v)) {
+    vertex.color <- vertex.color[v]
+  }
+  vertex.size <- 1/150 * params("vertex", "size")
+  if (length(vertex.size) != 1 && !is.null(v)) {
+    vertex.size <- vertex.size[v]
+  }
+
+  vertex.size2 <- 1/150 * params("vertex", "size2")
+  if (length(vertex.size2) != 1 && !is.null(v)) {
+    vertex.size2 <- vertex.size2[v]
+  }
+
+  x <- coords[,1]
+  y <- coords[,2]
+  dx <- vertex.size2/2
+  dy <- vertex.size/2
+  polygon(x = c(x - dx, x + dx, x + dx+dy, x + dx, x - dx, x- dx - dy),
+          y = c(y - dy, t - dy, y, y + dy, y + dy, y),
+          col = vertex.color)
+}
+
--- pkg/WayFindR/R/zzz.R
+++ pkg/WayFindR/R/zzz.R
@@ -3,5 +3,16 @@
   igraph::add_shape("ellipse",
                     clip = igraph::shapes("rectangle")$clip,
                     plot = igraphEllipse)
-  invisible("ellipse")
+  igraph::add_shape("hexagon",
+                    clip = igraph::shapes("rectangle")$clip,
+                    plot = igraphHexagon)
+  invisible("shaping")
+}
+
+if (FALSE) {
+  library(stringi)
+  hx <- stri_unescape_unicode("\u2b22")
+  plot(1, 1, pch = hx, cex = 4)
+  text(1, 1, labels = hx, font = 5)
+  text(0.65, 0.65, letters, font = 5, adj = 1)
 }
