f43241240b27bd8d8c3b69070e525f5ed1a10a66
krcoombes (Kevin Coombes) <kevin.r.coombes@gmail.com>
2025-04-08 17:56:48 +0000

    Cleaned up DESCRIPTION and man page links. Added documentation for as.graphNEL.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/cytangle@140 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/WayFindR/tests/11-testkgml.R
+++ pkg/WayFindR/tests/11-testkgml.R
@@ -1,9 +1,16 @@
 library(WayFindR)
+library(Rgraphviz)
 ## input directly from file
 xmlfile <- system.file("pathways/kegg_hsa00510.xml", package = "WayFindR")
 graf <- KGMLtoIgraph(xmlfile)
 set.seed(24086)
 L <- igraph::layout_with_kk(graf)
 plot(graf, layout = L)
-## FAILS becassue of multiple edges:
-## RA <- as.graphNEL(graf)
+## FAILS because of multiple edges:
+try( RA <- as.graphNEL(graf) )
+
+xmlfile <- system.file("pathways/WP3850.gpml", package = "WayFindR")
+G <- GPMLtoIgraph(xmlfile)
+set.seed(13579)
+GN <- as.graphNEL(G)
+plot(GN)
